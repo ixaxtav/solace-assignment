ea816f572db4b51b4310384f3b8d1f65df209707
Ixax Tavarez <ixaxtav@gmail.com>
2025-09-20 11:02:15 -0400

    fix: enable database selection in GET request

--- src/app/api/advocates/route.ts
+++ src/app/api/advocates/route.ts
@@ -1,12 +1,9 @@
 import db from "../../../db";
 import { advocates } from "../../../db/schema";
-import { advocateData } from "../../../db/seed/advocates";
 
-export async function GET() {
-  // Uncomment this line to use a database
-  // const data = await db.select().from(advocates);
-
-  const data = advocateData;
+export async function GET(request: Request) {
+ 
+  const data = await db.select().from(advocates);
 
   return Response.json({ data });
 }
